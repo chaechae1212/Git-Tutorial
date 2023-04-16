88789de5322e12eae11a89a482ae1a61c95bf486
chaechae1212 <julie1392@naver.com>
2023-04-16 22:12:15 +0900

    Add hw6.py[hw6.py]

---
+++ hw6.py
@@ -0,0 +1,15 @@
+for i in range(1,10):
+    for j in range(2,6):
+         print(f'{j}X{i}={i*j}\t',end='')
+    print('')
+print('')
+
+
+for i in range(1,10):
+    for j in range(6,10):
+           print(f'{j}X{i}={i*j}\t',end='')
+    print('')
+   
+        
+        
+         
